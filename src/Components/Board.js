23ce54e8d50fa7f44ac6ec2aa74ff7aee63b3505
AndreyKatrusha <andrey.katrusha@litslink.com>
2018-12-26 15:42:35 +0200

    revrite to HOC

---
+++ src/Components/Board.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Square from "./Sqeare";
+
+
+function renderSquare(i, squares, onClick) {
+    return (
+        <Square
+            value={squares[i]}
+            onClick={()=> onClick(i)}
+        />
+    );
+}
+function Board({squares, onClick}) {
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0, squares, onClick)}
+                {renderSquare(1, squares, onClick)}
+                {renderSquare(2, squares, onClick)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3, squares, onClick)}
+                {renderSquare(4,  squares, onClick)}
+                {renderSquare(5,  squares, onClick)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6, squares, onClick)}
+                {renderSquare(7, squares, onClick)}
+                {renderSquare(8, squares, onClick)}
+            </div>
+        </div>
+    );
+}
+
+export default Board;
