a5e1f7fc1b3512b83f4fe63acc40638fc49aae30
AndreyKatrusha <andrey.katrusha@litslink.com>
2018-12-27 17:21:41 +0200

    add hook component

--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Board from './Components/Board';
 function Game2() {
- const [history, changeHistory] = useState({
-     history: [ { squares: Array(9).fill(null) } ],
-     stepNumber: 0,
-     xIsNext: true
- });
+    const [historyState, changeHistory] = useState({
+        history: [{ squares: Array(9).fill(null) }],
+        stepNumber: 0,
+        xIsNext: true
+    });
 
- const handleClick = (i) => {
-    // history.squares
- }
+    const handleClick = (i) => {
+        const history = historyState.history.slice(0, historyState.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = historyState.xIsNext ? "X" : "O";
+        changeHistory({ history: squares, stepNumber: history.length, xIsNext: !historyState.xIsNext });
+    };
+    const jumpTo = (step) => {
+        changeHistory({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        });
+    };
+    const moves = historyState.history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+    const current = historyState.history[historyState.stepNumber];
+    const winner = calculateWinner(current.squares);
+    let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            status = "Next player: " + (historyState.xIsNext ? "X" : "O");
+        }
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={i => handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
 }
 
 class Game extends React.Component {
@@ -95,7 +140,7 @@ class Game extends React.Component {
 
 // ========================================
 
-export default Game;
+export default Game2;
 
 function calculateWinner(squares) {
     const lines = [
